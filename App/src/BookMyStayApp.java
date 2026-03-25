5ab94b3c2067da17083a6e436033cda2fc22f848
Rohith20062509 <sunny20062509@gmail.com>
2026-03-25 09:09:45 +0530

    UC1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,7 @@
+// Main Driver
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Booking Mangement System");
+        System.out.println("System Initialized System");
+    }
+}
